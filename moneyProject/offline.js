071418d3c2bf0fd411d247869bb5b1acec47e60e
meangpu <51825872+meangpu@users.noreply.github.com>
2020-12-18 10:55:59 +0700

    www

---
+++ moneyProject/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1608263546,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/s_1-sheet0.png",
+		"images/s_5-sheet0.png",
+		"images/s_10-sheet0.png",
+		"images/s_2-sheet0.png",
+		"images/s_20-sheet0.png",
+		"images/s_50-sheet0.png",
+		"images/s_100-sheet0.png",
+		"images/s_500-sheet0.png",
+		"images/g_ground.png",
+		"images/s_getmoney-sheet0.png",
+		"images/s_1000-sheet0.png",
+		"images/g_bg1.png",
+		"images/s_delall-sheet0.png",
+		"images/g_black.png",
+		"images/s_shake-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
